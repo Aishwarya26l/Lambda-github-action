8eee111de4f3814118383faa72be5a37a4f1735a
Aishwarya Lakshminarasimhan <aishwaryarln@gmail.com>
2019-09-18 14:26:34 +0800

    Added aws config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh -l
 
 sh -c "echo Hello world my name is $MY_NAME"
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
+export AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN
+aws ec2 describe-instances --output table --region us-east-1
