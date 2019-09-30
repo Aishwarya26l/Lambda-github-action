f65e175bf26bb33bcda988930469459562286523
Aishwarya Lakshminarasimhan <aishwaryarln@gmail.com>
2019-09-30 11:21:31 +0800

    Testing tar file upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,12 @@ export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
 export AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN
 export APINAME="$LAMBDA_FUNC_NAME-API"
+export OVERLAY_S3URL="s3://${BUCKET_NAME}/${LAMBDA_FUNC_NAME}/lambda-deploy.tgz"
 
 
-# rm -f lambda-deploy.zip
-# overlay_s3url="s3://${BUCKET_NAME}/${LAMBDA_FUNC_NAME}/lambda-deploy.tgz"
-# aws s3 cp --acl public-read lambda-nbconvert-overlay.tgz "$overlay_s3url"
-# sed -i "s!^OVERLAY_S3URL *=.*!OVERLAY_S3URL = '$overlay_s3url'!" main.py
-# zip -ry9 lambda-nbconvert.zip index.html main.py s3cat main.78615eaa.js build
+rm -f lambda-deploy.zip
+tar -czvf lambda-deploy-overlay.tgz ./
+aws s3 cp --acl public-read lambda-deploy-overlay.tgz "$OVERLAY_S3URL"
 zip -r lambda-deploy.zip ./
 
 aws cloudformation validate-template \
