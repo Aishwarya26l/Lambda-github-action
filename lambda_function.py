10db96c2e3f16dd2103d40172eb5efa77a336d75
Aishwarya Lakshminarasimhan <aishwaryarln@gmail.com>
2019-09-30 13:40:57 +0800

    Testing stack update

--- lambda_function.py
+++ lambda_function.py
@@ -1,10 +1,11 @@
 import json
 
+
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "headers": {
             'Content-Type': 'text/html',
         },
-        "body": "Hi there"
+        "body": "Hi there, I'm New!"
     }
