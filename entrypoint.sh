c83b5b3ab74de4331273809114874e98584443c9
Aishwarya Lakshminarasimhan <aishwaryarln@gmail.com>
2019-09-30 11:08:49 +0800

    Test redeploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,21 @@ export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
 export AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN
 export APINAME="$LAMBDA_FUNC_NAME-API"
 
+
+# rm -f lambda-deploy.zip
+# overlay_s3url="s3://${BUCKET_NAME}/${LAMBDA_FUNC_NAME}/lambda-deploy.tgz"
+# aws s3 cp --acl public-read lambda-nbconvert-overlay.tgz "$overlay_s3url"
+# sed -i "s!^OVERLAY_S3URL *=.*!OVERLAY_S3URL = '$overlay_s3url'!" main.py
+# zip -ry9 lambda-nbconvert.zip index.html main.py s3cat main.78615eaa.js build
+zip -r lambda-deploy.zip ./
+
 aws cloudformation validate-template \
     --template-body file://template.yaml
+
+aws cloudformation package \
+   --template-file template.yaml \
+   --output-template-file packaged.yaml \
+   --s3-bucket "${BUCKET_NAME}"
 
 aws cloudformation deploy \
     --stack-name ${LAMBDA_FUNC_NAME} \
